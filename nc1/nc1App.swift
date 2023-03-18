d5a70d68355dd153d8f915ac6f16e95d1675c278
rosatiara <rosatiaragaluh@gmail.com>
2023-03-18 15:56:27 +0700

    Initial Commit

---
+++ nc1/nc1App.swift
@@ -0,0 +1,17 @@
+//
+//  nc1App.swift
+//  nc1
+//
+//  Created by Rosa Tiara Galuh on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct nc1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
