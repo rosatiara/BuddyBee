7ba467478ef266df9564890197f655555624ec2d
Rio Johanes <72259697+riojohanes@users.noreply.github.com>
2023-03-24 19:50:20 +0700

    Sharesave button + emoji
    Share button and save button + emoji randomizer

---
+++ nc1/Models/Emojis.swift
@@ -0,0 +1,12 @@
+//
+//  Emojis.swift
+//  nc1
+//
+//  Created by Rio Johanes Sumolang on 24/03/23.
+//
+
+import Foundation
+
+class Emojis: NSObject {
+    var emoji: NSArray = ["😊", "😇", "😌", "🙂", "😉", "😍", "😘", "😜", "😝", "😛", "🤪", "🤩", "🥳", "😎", "🤓", "😸", "😺"]
+}
--- nc1/Views/CameraPageView.swift
+++ nc1/Views/CameraPageView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CameraPageView: View {
     let learner = learners.randomElement()!
+    let emoji = emojis.randomElement()!
+    let fontsize: CGFloat = 50.0
     @StateObject var camera = CameraModel()
     
     @State private var isConfettiAnimating = false
@@ -18,6 +20,18 @@ struct CameraPageView: View {
             LinearGradient(gradient: Gradient(colors: [(Color(0xFEDF3F)), (Color(0xFED43F))]), startPoint: .top, endPoint: .bottom)
             //Color(0xFEDF3F)
                 .edgesIgnoringSafeArea(.all)
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing){
+                    Button(action:{},
+                            label:{
+                            Image(systemName: "square.and.arrow.down")
+                            })
+                            Button(action: actionSheet,
+                                label:{
+                                Image(systemName: "square.and.arrow.up")
+                                })
+                    }
+                }
             VStack(alignment: .center){
                 VStack {
                     Text("Take a selfie with.. \n ")
@@ -29,7 +43,12 @@ struct CameraPageView: View {
                         .font(.title)
                 }.multilineTextAlignment(.center)
                 .foregroundColor(.black)
-                CameraView()
+                ZStack {
+                    CameraView()
+                    Text(emoji)
+                    .font(.system(size: fontsize))
+                    .padding()
+                }
                 ZStack { // Shutter Button
                     Circle()
                         .fill(.black)
@@ -56,7 +75,18 @@ struct CameraPageView: View {
             }
         }
     }
+    func actionSheet() {
+        guard let urlShare = URL(string:"https://developer.apple.com/xcode/swiftui/")
+        else {return }
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let window = windowScene.windows.first {
+            let activityVC = UIActivityViewController(activityItems: [urlShare], applicationActivities: nil)
+            window.rootViewController?.present(activityVC, animated: true, completion: nil)
+        }
+    }
 }
+
+let emojis = ["😊", "😇", "😌", "🙂", "😉", "😍", "😘", "😜", "😝", "😛", "🤪", "🤩", "🥳", "😎", "🤓", "😸", "😺", "😷"]
 
 struct CameraPageView_Previews: PreviewProvider {
     static var previews: some View {
